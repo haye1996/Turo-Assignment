bb08ca35753b97e78d0dbc590cf86fc46ec46990
haitong ye <haitong.ye.sde@gmail.com>
2025-07-17 10:10:09 -0700

    Enhancement 4: Serve /config.html from ConfigMap

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -26,6 +26,18 @@ resource "kubernetes_deployment" "app" {
           port {
             container_port = 80
           }
+          volume_mount {
+            name       = "config-html"
+            mount_path = "/usr/share/nginx/html/config.html"
+            sub_path   = "config.html"
+            read_only  = true
+          }
+        }
+        volume {
+          name = "config-html"
+          config_map {
+            name = "config-html"
+          }
         }
       }
     }
@@ -46,5 +58,27 @@ resource "kubernetes_service" "app" {
       target_port = 80
     }
     type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_config_map" "config_html" {
+  metadata {
+    name      = "config-html"
+    namespace = var.namespace
+  }
+  data = {
+    config.html = <<EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ConfigMap Page</title>
+</head>
+<body>
+    <h1>This page is served from a Kubernetes ConfigMap!</h1>
+    <p>You can update this content by editing the ConfigMap, no image rebuild required.</p>
+</body>
+</html>
+EOF
   }
 } 
